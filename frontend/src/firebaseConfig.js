3c75071585ca033605193f9624ce968d6ba111cb
GitMicaFri <micaela.fristedt@gmail.com>
2025-01-12 13:14:13 +0100

    Restructuring project

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -1,11 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: 'receptsamling-d1f75.firebaseapp.com',
     projectId: 'receptsamling-d1f75',
     storageBucket: 'receptsamling-d1f75.firebasestorage.app',
@@ -13,7 +7,4 @@ const firebaseConfig = {
     appId: '1:228833696252:web:3098ae3a9b9bcd45887a79',
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-export default app;
+export default firebaseConfig;
